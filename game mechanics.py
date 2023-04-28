9c11d4062c9a23bacc6b69a0b9f7ca1d645b1c91
BEA CZARINA SOTELO <120117455+beaczar@users.noreply.github.com>
2023-04-28 21:25:55 +0800

    Add files via upload

---
+++ game mechanics.py
@@ -0,0 +1,60 @@
+title = """WELCOME TO THE FINGAME
+Here's the game mechanics
+_________________________________________________________________________________________________________________"""
+print(title)
+a = """First and foremost, the game aims to keep your wellbeing score as high as possible.
+Wellbeing is about staying well in all areas of your life.
+You can't affect your wellbeing score directly it's based on some other scores you have to think about:
+
+Fun score - this shows you how much fun you're having! You can raise your fun score by buying things to keep you busy 
+such as books or video games, or by doing fun activities like meeting friends or playing football.
+
+Health score - a measure of how good your health is.
+Doing sport and getting exercise is good for your health. If you don't do this enough, or you eat unhealthy food, 
+your health score will go down.
+
+Comfort score - this goes down whenever you do something that's hard work or a lot of effort, including exercise or 
+cooking for yourself. 
+But you can raise it with things that make life easier or more comfortable, such as a dishwasher or a winter coat.
+
+Satisfaction anything in the game that makes you feel good about yourself increases your satisfaction score. 
+Giving money to charity, reading a book or doing some exercise will all increase your satisfaction levels.
+___________________________________________________________________________________________________________________"""
+print(a)
+
+b = """
+___________________________________________________________________________________________________________________
+Most things in the game don't come for free! 
+Every month you'll get paid for doing your job, but you also have bills to pay - you have to decide what to spend 
+your left-over money on. 
+Your balance tells you how much money you've got in the bank.
+Every week you must make THREE choices for each of these things:
+
+Activities-what you spend your time doing
+Food - what you eat
+Transport - how you get around
+
+There are also lots of items you can buy to increase your scores - but you don't have to.
+Some items don't increase your scores, but they allow you to do other things for example you need to buy trainers 
+to go running. 
+You start the game with some items already.
+___________________________________________________________________________________________________________________"""
+print(b)
+
+c = """
+___________________________________________________________________________________________________________________
+Each week in the game lasts one turn. Once you've made all your choices for the week, you need to click the 'Go to 
+next week' button. 
+Then your scores will change before the next turn starts.
+
+To win, you need to stay in the game for 12 weeks. 
+If you run out of money before then, or your wellbeing drops too low, it's game over! 
+You need to make sure you always have enough money in the bank to pay all your bills. 
+After 12 weeks, you can carry on playing if you like.
+
+Don't forget to read your messages every week for advice on how you're doing, updates on your spending, 
+and other important news. 
+The Finance screen shows you all your fixed payments for the month, and a list of all your recent spending - 
+use it to plan how much you're going to spend.
+___________________________________________________________________________________________________________________"""
+print(c)
