c5cf28c0c360eb7c2511ad51529d653d311a46bd
jomaaanguit <22-09658@g.batstate-u.edu.ph>
2023-05-22 15:38:37 +0800

    Updated with lesser coupling and lesser content

--- draft.py
+++ draft.py
@@ -1,220 +1,226 @@
 import os
-import random
 
-def stats_threshold(energy, fun, health):
-    energy = min(energy, 100)
-    fun = min(fun, 100)
-    health = min(health, 100)
+class Game:
+    def __init__(self):
+        self.energy = 100
+        self.fun = 100
+        self.health = 100
+        self.net_worth = 20000
+        self.decision_counter = 0
 
-# Function to handle the food tasks
-def handle_food(energy, fun, health, net_worth, decision_counter):
-    os.system("cls")  # Clear screen
+    def clear_screen(self):
+        os.system("cls" if os.name == "nt" else "clear")
 
-    print("Food Options:")
-    print("1. Home Cooking")
-    print("2. Take Out")
-    print("3. Fast Food")
-    print("4. Fine Dining")
-
-    food_choice = input("Enter your choice (1-4): ")
-    print()
-
-    options = {
-        '1': {'energy_depletion': 10, 'fun_depletion': 10, 'health_depletion': 5, 'cost': 100, 'message': "You cooked a delicious meal at home."},
-        '2': {'energy_depletion': 5, 'fun_depletion': 10, 'health_depletion': 10, 'cost': 200, 'message': "You ordered take-out from a nearby karinderya."},
-        '3': {'energy_depletion': 5, 'fun_depletion': 5, 'health_depletion': 10, 'cost': 350, 'message': "You had a quick meal at a fast food restaurant."},
-        '4': {'energy_depletion': 10, 'fun_depletion': 0, 'health_depletion': 5, 'cost': 1000, 'message': "You enjoyed a luxurious fine dining experience."},
-    }
-
-    if food_choice in options:
-        energy -= options[food_choice]['energy_depletion']
-        fun -= options[food_choice]['fun_depletion']
-        health -= options[food_choice]['health_depletion']
-        net_worth -= options[food_choice]['cost']
-        print(options[food_choice]['message'])
-        print()
-        decision_counter += 1
-    else:
-        print("Invalid choice!")
-
-    stats_threshold(energy, fun, health)
-
-    return energy, fun, health, net_worth, decision_counter
-
-
-# Function to handle the leisure tasks
-def handle_leisure(energy, fun, health, net_worth, decision_counter):
-    os.system("cls")  # Clear screen
-
-    print("Leisure Options:")
-    print("1. Read a Book")
-    print("2. Play Video Games")
-    print("3. Chill in a Coffee Shop")
-    print("4. Watch a Movie in a Cinema")
-    print("5. Go for Shopping")
-    print("6. Attend a Concert")
-
-    leisure_choice = input("Enter your choice (1-6): ")
-    print()
-
-    options = {
-        '1': {'energy_depletion': 5, 'fun_depletion': 20, 'health_depletion': 0, 'cost': 0, 'message': "You immersed yourself in a captivating book."},
-        '2': {'energy_depletion': 10, 'fun_depletion': 15, 'health_depletion': 5, 'cost': 50, 'message': "You played video games and had a blast."},
-        '3': {'energy_depletion': 10, 'fun_depletion': 10, 'health_depletion': 5, 'cost': 200, 'message': "You sipped your favorite beverage in a cafe."},
-        '4': {'energy_depletion': 15, 'fun_depletion': 5, 'health_depletion': 10, 'cost': 500, 'message': "You watched a movie and had a great time."},
-        '5': {'energy_depletion': 20, 'fun_depletion': 5, 'health_depletion': 5, 'cost': 1000, 'message': "You spent money on the things you love."},
-        '6': {'energy_depletion': 40, 'fun_depletion': 0, 'health_depletion': 15, 'cost': 5000, 'message': "You enjoyed a musical concert."},
-    }
-
-    if leisure_choice in options:
-        energy -= options[leisure_choice]['energy_depletion']
-        fun -= options[leisure_choice]['fun_depletion']
-        health -= options[leisure_choice]['health_depletion']
-        net_worth -= options[leisure_choice]['cost']
-        print(options[leisure_choice]['message'])
-        print()
-        decision_counter += 1
-    else:
-        print("Invalid choice!")
-
-    stats_threshold(energy, fun, health)
-
-    return energy, fun, health, net_worth, decision_counter
-
-
-# Function to handle the exercise tasks
-def handle_exercise(energy, fun, health, net_worth, decision_counter):
-    os.system("cls")  # Clear screen
-
-    print("Exercise Options:")
-    print("1. Walking")
-    print("2. Running")
-    print("3. Home Workout")
-    print("4. Gym")
-
-    exercise_choice = input("Enter your choice (1-4): ")
-    print()
-
-    options = {
-        '1': {'energy_depletion': 5, 'fun_depletion': 15, 'health_increment': 10, 'cost': 0, 'message': "You went for a refreshing walk."},
-        '2': {'energy_depletion': 10, 'fun_depletion': 10, 'health_increment': 15, 'cost': 0, 'message': "You enjoyed a good run."},
-        '3': {'energy_depletion': 20, 'fun_depletion': 5, 'health_increment': 20, 'cost': 0, 'message': "You had a great time improving your fitness at home."},
-        '4': {'energy_depletion': 30, 'fun_depletion': 5, 'health_increment': 15, 'cost': 500, 'message': "You hit the gym to build those muscles."},
-    }
-
-    if exercise_choice in options:
-        energy -= options[exercise_choice]['energy_depletion']
-        fun -= options[exercise_choice]['fun_depletion']
-        health += options[exercise_choice]['health_increment']
-        print(options[exercise_choice]['message'])
-        print()
-        decision_counter += 1
-    else:
-        print("Invalid choice!")
-
-    stats_threshold(energy, fun, health)
-
-    return energy, fun, health, net_worth, decision_counter
-
-
-# Function to handle the transportation tasks
-def handle_transportation(energy, fun, health, net_worth, decision_counter):
-    os.system("cls")  # Clear screen
-
-    print("Transportation Options:")
-    print("1. Walk")
-    print("2. Bicycle")
-    print("3. Jeepney")
-    print("4. Car")
-
-    transportation_choice = input("Enter your choice (1-4): ")
-    print()
-
-    options = {
-        '1': {'energy_depletion': 15, 'fun_depletion': 5, 'health_depletion': 0, 'cost': 0, 'message': "You walked to your destination."},
-        '2': {'energy_depletion': 10, 'fun_depletion': 5, 'health_depletion': 0, 'cost': 0, 'message': "You rode a bicycle to your destination."},
-        '3': {'energy_depletion': 5, 'fun_depletion': 10, 'health_depletion': 5, 'cost': 50, 'message': "You used a jeepney to reach your destination."},
-        '4': {'energy_depletion': 0, 'fun_depletion': 5, 'health_depletion': 0, 'cost': 100, 'message': "You drove a car to your destination."},
-    }
-
-    if transportation_choice in options:
-        energy -= options[transportation_choice]['energy_depletion']
-        fun -= options[transportation_choice]['fun_depletion']
-        health -= options[transportation_choice]['health_depletion']
-        net_worth -= options[transportation_choice]['cost']
-        print(options[transportation_choice]['message'])
-        print()
-        decision_counter += 1
-    else:
-        print("Invalid choice!")
-
-    stats_threshold(energy, fun, health)
-
-    return energy, fun, health, net_worth, decision_counter
-
-
-# Main game loop
-def game_loop():
-    energy = 100
-    fun = 100
-    health = 100
-    net_worth = 10000
-    decision_counter = 0
-
-    while energy > 0 and fun > 0 and health > 0 and net_worth > 0:
-        os.system("cls")  # Clear screen
-
+    def print_stats(self):
         print("Well-Being Stats:")
-        print("Energy:", energy)
-        print("Fun:", fun)
-        print("Health:", health)
+        print("Energy:", self.energy)
+        print("Fun:", self.fun)
+        print("Health:", self.health)
         print("")
-        print("Net Worth:", net_worth)
-        print("Decisions:", decision_counter)
+        print("Net Worth:", self.net_worth)
+        print("Decisions:", self.decision_counter)
         print()
 
-        print("Options:")
-        print("1. Food")
-        print("2. Leisure")
-        print("3. Exercise")
-        print("4. Transportation")
-        print("5. Quit")
-
-        choice = input("Enter your choice (1-5): ")
+    def print_invalid_choice(self):
+        print("Invalid choice!")
         print()
 
-        if choice == '1':
-            energy, fun, health, net_worth, decision_counter = handle_food(energy, fun, health, net_worth, decision_counter)
-        elif choice == '2':
-            energy, fun, health, net_worth, decision_counter = handle_leisure(energy, fun, health, net_worth, decision_counter)
-        elif choice == '3':
-            energy, fun, health, net_worth, decision_counter = handle_exercise(energy, fun, health, net_worth, decision_counter)
-        elif choice == '4':
-            energy, fun, health, net_worth, decision_counter = handle_transportation(energy, fun, health, net_worth, decision_counter)
-        elif choice == '5':
-            break
+    def calculate_score(self):
+        return self.energy + self.fun + self.health + self.net_worth - self.decision_counter * 100
+
+    def handle_task(self, options):
+        self.clear_screen()
+        self.print_stats()
+
+        for key, value in options.items():
+            print(f"{key}. {value['name']}")
+
+        user_choice = input(f"Enter your choice (1-{len(options)}): ")
+        self.clear_screen()
+
+        if user_choice in options:
+            option = options[user_choice]
+            self.energy = min(self.energy + option['energy_change'], 100)
+            self.fun = min(self.fun + option['fun_change'], 100)
+            self.health = min(self.health + option['health_change'], 100)
+            self.net_worth -= option['cost']
+            print(option['message'])
+            print()
+            self.decision_counter += 1
         else:
-            print("Invalid choice!")
+            self.print_invalid_choice()
 
-    os.system("cls")  # Clear screen
+        if self.decision_counter % 5 == 0:
+            self.energy = min(self.energy + 30, 100)
+            self.fun = min(self.fun + 30, 100)
+            self.health = min(self.health + 30, 100)
 
-    print("Game Over!")
-    print("Final Stats:")
-    print("Energy:", energy)
-    print("Fun:", fun)
-    print("Health:", health)
-    print("")
-    print("Net Worth:", net_worth)
-    print("Decisions:", decision_counter)
-    print()
+        if self.decision_counter % 20 == 0 and self.decision_counter > 0:
+            self.net_worth -= 4750  # Deduct house bills such as rent, water, electricity, and internet
 
-    score = energy + fun + health + net_worth - decision_counter * 100
-    print("Score:", score)
+            print("House bills deducted from your net worth:")
+            print("- Rent: 2000")
+            print("- Water Bill: 500")
+            print("- Electric Bill: 750")
+            print("- Internet Bill: 1500")
+            print()
+
+    def game_loop(self):
+        while self.energy > 0 and self.fun > 0 and self.health > 0 and self.net_worth > 0:
+            self.print_stats()
+
+            print("Options:")
+            print("1. Food")
+            print("2. Leisure")
+            print("3. Exercise")
+            print("4. Transportation")
+            print("5. Quit")
+
+            choice = input("Enter your choice (1-5): ")
+            self.clear_screen()
+
+            if choice == '1':
+                self.handle_task(self.get_food_options())
+            elif choice == '2':
+                self.handle_task(self.get_leisure_options())
+            elif choice == '3':
+                self.handle_task(self.get_exercise_options())
+            elif choice == '4':
+                self.handle_task(self.get_transportation_options())
+            elif choice == '5':
+                break
+            else:
+                self.print_invalid_choice()
+
+        self.clear_screen()
+        self.print_stats()
+
+        print("Game Over!")
+        print("Final Stats:")
+        print("Energy:", self.energy)
+        print("Fun:", self.fun)
+        print("Health:", self.health)
+        print("Net Worth:", self.net_worth)
+        print("Decisions:", self.decision_counter)
+        print("Score:", self.calculate_score())
+
+    def get_food_options(self):
+        options = {
+            '1': {
+                'name': 'Eat a healthy meal',
+                'energy_change': 10,
+                'fun_change': -5,
+                'health_change': 10,
+                'cost': 50,
+                'message': 'You ate a healthy meal. Energy +10, Fun -5, Health +10.'
+            },
+            '2': {
+                'name': 'Grab a quick snack',
+                'energy_change': 5,
+                'fun_change': 0,
+                'health_change': -5,
+                'cost': 10,
+                'message': 'You grabbed a quick snack. Energy +5, Fun +0, Health -5.'
+            },
+            '3': {
+                'name': 'Skip a meal',
+                'energy_change': -10,
+                'fun_change': 0,
+                'health_change': -5,
+                'cost': 0,
+                'message': 'You skipped a meal. Energy -10, Fun +0, Health -5.'
+            }
+        }
+        return options
+
+    def get_leisure_options(self):
+        options = {
+            '1': {
+                'name': 'Watch a movie',
+                'energy_change': -10,
+                'fun_change': 20,
+                'health_change': 0,
+                'cost': 50,
+                'message': 'You watched a movie. Energy -10, Fun +20, Health +0.'
+            },
+            '2': {
+                'name': 'Go to a party',
+                'energy_change': -20,
+                'fun_change': 30,
+                'health_change': -10,
+                'cost': 100,
+                'message': 'You went to a party. Energy -20, Fun +30, Health -10.'
+            },
+            '3': {
+                'name': 'Read a book',
+                'energy_change': -5,
+                'fun_change': 5,
+                'health_change': 0,
+                'cost': 20,
+                'message': 'You read a book. Energy -5, Fun +5, Health +0.'
+            }
+        }
+        return options
+
+    def get_exercise_options(self):
+        options = {
+            '1': {
+                'name': 'Go for a run',
+                'energy_change': -20,
+                'fun_change': 10,
+                'health_change': 20,
+                'cost': 0,
+                'message': 'You went for a run. Energy -20, Fun +10, Health +20.'
+            },
+            '2': {
+                'name': 'Do yoga',
+                'energy_change': -10,
+                'fun_change': 5,
+                'health_change': 15,
+                'cost': 0,
+                'message': 'You did yoga. Energy -10, Fun +5, Health +15.'
+            },
+            '3': {
+                'name': 'Lift weights',
+                'energy_change': -15,
+                'fun_change': 5,
+                'health_change': 25,
+                'cost': 0,
+                'message': 'You lifted weights. Energy -15, Fun +5, Health +25.'
+            }
+        }
+        return options
+
+    def get_transportation_options(self):
+        options = {
+            '1': {
+                'name': 'Take the bus',
+                'energy_change': -5,
+                'fun_change': 0,
+                'health_change': 0,
+                'cost': 5,
+                'message': 'You took the bus. Energy -5, Fun +0, Health +0.'
+            },
+            '2': {
+                'name': 'Ride a bike',
+                'energy_change': -10,
+                'fun_change': 5,
+                'health_change': 10,
+                'cost': 0,
+                'message': 'You rode a bike. Energy -10, Fun +5, Health +10.'
+            },
+            '3': {
+                'name': 'Drive a car',
+                'energy_change': -15,
+                'fun_change': 10,
+                'health_change': 5,
+                'cost': 20,
+                'message': 'You drove a car. Energy -15, Fun +10, Health +5.'
+            }
+        }
+        return options
 
 
-# Start the game
-os.system("cls") 
-print("INSERT INTRO NINA BEA AT VINCE")
-print()
-input("Press Enter to start the game...")
-game_loop()
+# Create and start the game
+game = Game()
+game.game_loop()
