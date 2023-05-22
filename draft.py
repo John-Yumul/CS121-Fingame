a7917f1b1f863cc4191121acd3323159ace2f3b8
jomaaanguit <22-09658@g.batstate-u.edu.ph>
2023-05-22 16:27:44 +0800

    Updated and made the code concise

--- draft.py
+++ draft.py
@@ -13,20 +13,23 @@ class Game:
 
     def print_stats(self):
         print("Well-Being Stats:")
-        print("Energy:", self.energy)
-        print("Fun:", self.fun)
-        print("Health:", self.health)
-        print("")
-        print("Net Worth:", self.net_worth)
-        print("Decisions:", self.decision_counter)
-        print()
+        print(f"Energy: {self.energy}")
+        print(f"Fun: {self.fun}")
+        print(f"Health: {self.health}\n")
+        print(f"Net Worth: {self.net_worth}")
+        print(f"Decisions: {self.decision_counter}\n")
 
     def print_invalid_choice(self):
-        print("Invalid choice!")
-        print()
+        print("Invalid choice!\n")
 
     def calculate_score(self):
-        return self.energy + self.fun + self.health + self.net_worth - self.decision_counter * 100
+        return (
+            self.energy
+            + self.fun
+            + self.health
+            + self.net_worth
+            - self.decision_counter * 100
+        )
 
     def handle_task(self, options):
         self.clear_screen()
@@ -40,12 +43,11 @@ class Game:
 
         if user_choice in options:
             option = options[user_choice]
-            self.energy = min(self.energy + option['energy_change'], 100)
-            self.fun = min(self.fun + option['fun_change'], 100)
-            self.health = min(self.health + option['health_change'], 100)
-            self.net_worth -= option['cost']
-            print(option['message'])
-            print()
+            self.energy = min(self.energy + option["energy_change"], 100)
+            self.fun = min(self.fun + option["fun_change"], 100)
+            self.health = min(self.health + option["health_change"], 100)
+            self.net_worth -= option["cost"]
+            print(option["message"] + "\n")
             self.decision_counter += 1
         else:
             self.print_invalid_choice()
@@ -62,11 +64,12 @@ class Game:
             print("- Rent: 2000")
             print("- Water Bill: 500")
             print("- Electric Bill: 750")
-            print("- Internet Bill: 1500")
-            print()
+            print("- Internet Bill: 1500\n")
 
     def game_loop(self):
-        while self.energy > 0 and self.fun > 0 and self.health > 0 and self.net_worth > 0:
+        while all(
+            [self.energy > 0, self.fun > 0, self.health > 0, self.net_worth > 0]
+        ):
             self.print_stats()
 
             print("Options:")
@@ -79,16 +82,17 @@ class Game:
             choice = input("Enter your choice (1-5): ")
             self.clear_screen()
 
-            if choice == '1':
-                self.handle_task(self.get_food_options())
-            elif choice == '2':
-                self.handle_task(self.get_leisure_options())
-            elif choice == '3':
-                self.handle_task(self.get_exercise_options())
-            elif choice == '4':
-                self.handle_task(self.get_transportation_options())
-            elif choice == '5':
+            option_mapping = {
+                "1": self.get_food_options,
+                "2": self.get_leisure_options,
+                "3": self.get_exercise_options,
+                "4": self.get_transportation_options,
+            }
+
+            if choice == "5":
                 break
+            elif choice in option_mapping:
+                self.handle_task(option_mapping[choice]())
             else:
                 self.print_invalid_choice()
 
@@ -97,130 +101,137 @@ class Game:
 
         print("Game Over!")
         print("Final Stats:")
-        print("Energy:", self.energy)
-        print("Fun:", self.fun)
-        print("Health:", self.health)
-        print("Net Worth:", self.net_worth)
-        print("Decisions:", self.decision_counter)
-        print("Score:", self.calculate_score())
+        print(f"Energy: {self.energy}")
+        print(f"Fun: {self.fun}")
+        print(f"Health: {self.health}")
+        print(f"Net Worth: {self.net_worth}")
+        print(f"Decisions: {self.decision_counter}")
+        print(f"Score: {self.calculate_score()}")
+
+    @staticmethod
+    def print_option_message(option):
+        print(f"You {option['message']}\n")
+
+    @staticmethod
+    def create_options(options):
+        return {str(index + 1): option for index, option in enumerate(options)}
 
     def get_food_options(self):
-        options = {
-            '1': {
-                'name': 'Eat a healthy meal',
-                'energy_change': 10,
-                'fun_change': -5,
-                'health_change': 10,
-                'cost': 50,
-                'message': 'You ate a healthy meal. Energy +10, Fun -5, Health +10.'
+        options = [
+            {
+                "name": "Eat a healthy meal",
+                "energy_change": 10,
+                "fun_change": -5,
+                "health_change": 10,
+                "cost": 50,
+                "message": "ate a healthy meal. Energy +10, Fun -5, Health +10.",
             },
-            '2': {
-                'name': 'Grab a quick snack',
-                'energy_change': 5,
-                'fun_change': 0,
-                'health_change': -5,
-                'cost': 10,
-                'message': 'You grabbed a quick snack. Energy +5, Fun +0, Health -5.'
+            {
+                "name": "Grab a quick snack",
+                "energy_change": 5,
+                "fun_change": 0,
+                "health_change": -5,
+                "cost": 10,
+                "message": "grabbed a quick snack. Energy +5, Fun +0, Health -5.",
             },
-            '3': {
-                'name': 'Skip a meal',
-                'energy_change': -10,
-                'fun_change': 0,
-                'health_change': -5,
-                'cost': 0,
-                'message': 'You skipped a meal. Energy -10, Fun +0, Health -5.'
-            }
-        }
-        return options
+            {
+                "name": "Skip a meal",
+                "energy_change": -10,
+                "fun_change": 0,
+                "health_change": -5,
+                "cost": 0,
+                "message": "skipped a meal. Energy -10, Fun +0, Health -5.",
+            },
+        ]
+        return self.create_options(options)
 
     def get_leisure_options(self):
-        options = {
-            '1': {
-                'name': 'Watch a movie',
-                'energy_change': -10,
-                'fun_change': 20,
-                'health_change': 0,
-                'cost': 50,
-                'message': 'You watched a movie. Energy -10, Fun +20, Health +0.'
+        options = [
+            {
+                "name": "Watch a movie",
+                "energy_change": -10,
+                "fun_change": 20,
+                "health_change": 0,
+                "cost": 50,
+                "message": "watched a movie. Energy -10, Fun +20, Health +0.",
             },
-            '2': {
-                'name': 'Go to a party',
-                'energy_change': -20,
-                'fun_change': 30,
-                'health_change': -10,
-                'cost': 100,
-                'message': 'You went to a party. Energy -20, Fun +30, Health -10.'
+            {
+                "name": "Go to a party",
+                "energy_change": -20,
+                "fun_change": 30,
+                "health_change": -10,
+                "cost": 100,
+                "message": "went to a party. Energy -20, Fun +30, Health -10.",
             },
-            '3': {
-                'name': 'Read a book',
-                'energy_change': -5,
-                'fun_change': 5,
-                'health_change': 0,
-                'cost': 20,
-                'message': 'You read a book. Energy -5, Fun +5, Health +0.'
-            }
-        }
-        return options
+            {
+                "name": "Read a book",
+                "energy_change": -5,
+                "fun_change": 5,
+                "health_change": 0,
+                "cost": 20,
+                "message": "read a book. Energy -5, Fun +5, Health +0.",
+            },
+        ]
+        return self.create_options(options)
 
     def get_exercise_options(self):
-        options = {
-            '1': {
-                'name': 'Go for a run',
-                'energy_change': -20,
-                'fun_change': 10,
-                'health_change': 20,
-                'cost': 0,
-                'message': 'You went for a run. Energy -20, Fun +10, Health +20.'
+        options = [
+            {
+                "name": "Go for a run",
+                "energy_change": -20,
+                "fun_change": 10,
+                "health_change": 20,
+                "cost": 0,
+                "message": "went for a run. Energy -20, Fun +10, Health +20.",
             },
-            '2': {
-                'name': 'Do yoga',
-                'energy_change': -10,
-                'fun_change': 5,
-                'health_change': 15,
-                'cost': 0,
-                'message': 'You did yoga. Energy -10, Fun +5, Health +15.'
+            {
+                "name": "Do yoga",
+                "energy_change": -10,
+                "fun_change": 5,
+                "health_change": 15,
+                "cost": 0,
+                "message": "did yoga. Energy -10, Fun +5, Health +15.",
             },
-            '3': {
-                'name': 'Lift weights',
-                'energy_change': -15,
-                'fun_change': 5,
-                'health_change': 25,
-                'cost': 0,
-                'message': 'You lifted weights. Energy -15, Fun +5, Health +25.'
-            }
-        }
-        return options
+            {
+                "name": "Lift weights",
+                "energy_change": -15,
+                "fun_change": 5,
+                "health_change": 25,
+                "cost": 0,
+                "message": "lifted weights. Energy -15, Fun +5, Health +25.",
+            },
+        ]
+        return self.create_options(options)
 
     def get_transportation_options(self):
-        options = {
-            '1': {
-                'name': 'Take the bus',
-                'energy_change': -5,
-                'fun_change': 0,
-                'health_change': 0,
-                'cost': 5,
-                'message': 'You took the bus. Energy -5, Fun +0, Health +0.'
+        options = [
+            {
+                "name": "Take a taxi",
+                "energy_change": -5,
+                "fun_change": 10,
+                "health_change": 0,
+                "cost": 50,
+                "message": "took a taxi. Energy -5, Fun +10, Health +0.",
             },
-            '2': {
-                'name': 'Ride a bike',
-                'energy_change': -10,
-                'fun_change': 5,
-                'health_change': 10,
-                'cost': 0,
-                'message': 'You rode a bike. Energy -10, Fun +5, Health +10.'
+            {
+                "name": "Ride a bike",
+                "energy_change": -15,
+                "fun_change": 10,
+                "health_change": 10,
+                "cost": 0,
+                "message": "rode a bike. Energy -15, Fun +10, Health +10.",
             },
-            '3': {
-                'name': 'Drive a car',
-                'energy_change': -15,
-                'fun_change': 10,
-                'health_change': 5,
-                'cost': 20,
-                'message': 'You drove a car. Energy -15, Fun +10, Health +5.'
-            }
-        }
-        return options
+            {
+                "name": "Walk",
+                "energy_change": -10,
+                "fun_change": 5,
+                "health_change": 15,
+                "cost": 0,
+                "message": "walked. Energy -10, Fun +5, Health +15.",
+            },
+        ]
+        return self.create_options(options)
 
 
-# Create and start the game
 game = Game()
 game.game_loop()
